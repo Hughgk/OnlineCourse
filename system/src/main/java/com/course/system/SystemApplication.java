d0cebf992160abd8c5115dc8aa8d43925d20e462
黄光奎 <991325480@qq.com>
2020-10-27 09:51:31 +0800

    1、system集成server模块(10/27)

--- system/src/main/java/com/course/system/SystemApplication.java
+++ system/src/main/java/com/course/system/SystemApplication.java
@@ -1,19 +1,21 @@
 package com.course.system;
 
 
-import com.course.server.service.TestService;
+
 import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
 import javax.annotation.Resource;
 
 @SpringBootApplication
 @EnableEurekaClient
+@ComponentScan("com.course")
 @MapperScan("com.course.server.mapper")
 public class SystemApplication {
 
